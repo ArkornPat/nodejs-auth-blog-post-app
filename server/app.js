69b3983a4148456a4cdefedf2ae1b39816bc4774
Arkorn Pattanavibul <arkorn.pat@gmail.com>
2024-11-19 13:41:55 +0700

    feat: add logout and middleware check token

--- server/app.js
+++ server/app.js
@@ -6,13 +6,13 @@ import { client } from "./utils/db.js";
 import authRouter from "./apps/auth.js";
 import dotenv from "dotenv";
 
+
 async function init() {
   const app = express();
   const port = 4000;
   dotenv.config();
 
   await client.connect();
-
   app.use(cors());
   app.use(bodyParser.json());
   app.use("/posts", postRouter);
